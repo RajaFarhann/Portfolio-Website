c327d8220c1fde0088f8a898e028afc23cf67feb
Raja Muhammad Farhan Zahputra <raja.farhan0503@gmail.com>
2026-02-16 00:19:50 +0700

    chore: move to specific folder data

---
+++ src/data/projectsData.js
@@ -0,0 +1,68 @@
+export const projects = [
+  {
+    title: "Infinite Track",
+    description:
+      "Infinite Track is an application designed to assist interns, employees, and management in efficiently managing attendance and leave requests. Previously handled manually with lengthy processes, the system now streamlines attendance recording and leave management, making the process faster, more transparent, and easier to track.",
+    tech: [
+      { name: "Kotlin", icon: "devicon-kotlin-plain colored" },
+      { name: "Jetpack Compose", icon: "devicon-jetpackcompose-plain colored" },
+      { name: "MySQL", icon: "devicon-mysql-plain colored" },
+      { name: "Node.Js", icon: "devicon-nodejs-plain colored" },
+      { name: "Express.Js", icon: "devicon-express-original" },
+    ],
+    image: "/projects/InfiniteTrack.jpg",
+    github: "",
+    link: "https://exhibition-infinite-learning.super.site/1624fb0d7d0a8061a9a6cf20feb151b6",
+  },
+  {
+    title: "Portfolio Website",
+    description:
+      "A personal portfolio website designed to introduce myself, showcase selected projects, and highlight my skills and experience. This website also provides access to my CV and contact information, allowing recruiters and collaborators to easily explore my work and get in touch.",
+    tech: [
+      { name: "Javascript", icon: "devicon-javascript-plain colored" },
+      { name: "React.Js", icon: "devicon-react-original colored" },
+      { name: "Next.Js", icon: "devicon-nextjs-plain" },
+      { name: "Tailwind CSS", icon: "devicon-tailwindcss-original colored" },
+    ],
+    image: "/projects/WebsitePortfolio.png",
+    github: "https://github.com/RajaFarhann/Portfolio-Website",
+    link: "https://rajafarhan.vercel.app/",
+  },
+  {
+    title: "Sporking",
+    description:
+      "An Android Application designed to provide comprehensive sports-related information and community engagement. The MVP (Minimum Viable Product) feature is Booking, enabling users to discover nearby sports fields, review available facilities in detail, and seamlessly make reservations and payments all within the application.",
+    tech: [
+      { name: "Kotlin", icon: "devicon-kotlin-plain colored" },
+      { name: "Jetpack Compose", icon: "devicon-android-plain colored" },
+    ],
+    image: "/projects/Sporking.png",
+    github: "https://github.com/RajaFarhann/Mobile-Sporking",
+    link: "",
+  },
+  {
+    title: "Savor Craft",
+    description:
+      "An application designed to enhance user experience through features such as recipe search, personalized recommendations, favorites, and category-based browsing. Its MVP (Minimum Viable Product) feature is ingredient scanning, which leverages photo recognition to detect food ingredients and provide relevant recipe suggestions.",
+    tech: [
+      { name: "Kotlin", icon: "devicon-kotlin-plain colored" },
+      { name: "Jetpack Compose", icon: "devicon-android-plain colored" },
+    ],
+    image: "/projects/SavorCraft.png",
+    github: "https://github.com/Wirasena-IL/SavorCraft",
+    link: "",
+  },
+  {
+    title: "Guide Me: Tour Guide Application",
+    description:
+      "A application designed to deliver comprehensive information on tourist attractions in Batam City. The application aims to assist both domestic and international tourists in navigating to destinations via their smartphones, ensuring an accessible and seamless travel experience.",
+    tech: [
+      { name: "Flutter", icon: "devicon-flutter-plain colored" },
+      { name: "Dart", icon: "devicon-dart-plain colored" },
+      { name: "MySQL", icon: "devicon-mysql-plain colored" },
+    ],
+    image: "/projects/GuideMe.png",
+    github: "",
+    link: "",
+  },
+]
